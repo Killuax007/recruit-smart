cf4eade50efda9d16a9be9e4fd9dc864786aef21
Killuax007 <pandamanoj872@gmail.com>
2025-05-13 14:46:53 +0530

    adding frontend ✅

---
+++ recruitsmart-frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      Raleway: ["Raleway"],
+    },
+    darkMode: "class",
+    extend: {
+      colors: {
+        primary: "#000000", // Uber's black theme
+        secondary: "#ffffff", // White
+        accent: "#ffc800", // Uber's yellow accent
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
